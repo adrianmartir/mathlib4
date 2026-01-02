75091f451a9ea510b01b8085675fe47fe6266d18
Adrian Marti <adrianmarti06@gmail.com>
2026-01-02 15:01:33 +0100

    Fill in missing proofs.

--- Mathlib/CategoryTheory/Operad.lean
+++ Mathlib/CategoryTheory/Operad.lean
@@ -158,16 +158,19 @@ class MonadProf (T : Monad Cat) extends FunctorProf T where
 
 lemma left_unit_obj {T : Monad Cat} {X} (a : T.obj X) :
     (T.μ.app _).toFunctor.obj ((T.η.app _).toFunctor.obj a) = a := by
-  sorry
+  rw [<- CategoryTheory.Cat.Hom.comp_obj, Monad.left_unit]
+  rfl
 
 lemma right_unit_obj {T : Monad Cat} {X} (a : T.obj X) :
     (T.μ.app _).toFunctor.obj ((T.map (T.η.app _)).toFunctor.obj a) = a := by
-  sorry
+  rw [<- CategoryTheory.Cat.Hom.comp_obj, Monad.right_unit]
+  rfl
 
 lemma mul_comp_obj {T : Monad Cat} {X} (a : T.obj (T.obj (T.obj X))) :
     (T.μ.app X).toFunctor.obj ((T.μ.app (T.obj X)).toFunctor.obj a)
       = (T.μ.app X).toFunctor.obj ((T.map (T.μ.app X)).toFunctor.obj a) := by
-  sorry
+  rw [<- CategoryTheory.Cat.Hom.comp_obj, <- Monad.assoc]
+  rfl
 
 variable (T : Monad Cat) [MonadProf T]
 
